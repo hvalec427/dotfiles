7470aeefb18bfc66cff38345fab7f8576e271674
Žiga Hvalec <zigahvalec@gmail.com>
2026-03-01 19:51:56 +0100

    add amethyst config
    add other zshrc config

---
+++ zsh/common.zsh
@@ -0,0 +1,55 @@
+# this is to handle jumping to commong paths
+typeset -A JUMP_PATHS
+JUMP_PATHS=(
+  dev "$HOME/dev"
+  dot "$HOME/dev/dotfiles"
+  dotfiles "$HOME/dev/dotfiles"
+  hvalec "$HOME/dev/zigahvalec"
+  .config "$HOME/.config/"
+)
+
+jump() {
+  if [[ $# -eq 0 ]]; then
+    cd "$HOME" || return
+    return
+  fi
+
+  case "$1" in
+    -h|--help)
+      printf 'Available jump targets:\n'
+      for key in "${(@k)JUMP_PATHS}"; do
+        printf '  %s -> %s\n' "$key" "${JUMP_PATHS[$key]}"
+      done
+      return
+      ;;
+  esac
+
+  local dest="${JUMP_PATHS[$1]}"
+  if [[ -n $dest ]]; then
+    cd "$dest" || return
+    return
+  fi
+
+  cd "$HOME/$1" || {
+    printf 'jump: target "%s" not found\n' "$1" >&2
+    return 1
+  }
+}
+
+# this section just get's added to zshrc
+
+# nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+# jenv
+eval "$(jenv init -)"
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
