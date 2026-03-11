46a9942f5ccb00d02da167066a0257895450e4f1
Žiga Hvalec <zigahvalec@gmail.com>
2026-03-11 18:31:13 +0100

    small improvements

--- config/nvim/.config/nvim/lua/plugins/theme.lua
+++ config/nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,19 +1,19 @@
-return { {
-  "sainnhe/sonokai",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    contrast = "medium",
-    background = "dark",
-    italic = true,
-    underline = true,
+return {
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "andromeda",
+      transparent_background = false,
+      term_colors = true,
+    },
+    config = function(_, opts)
+      vim.g.sonokai_style = opts.style
+      vim.g.sonokai_transparent_background = opts.transparent_background and 1 or 0
+      vim.g.sonokai_disable_terminal_colors = opts.term_colors and 0 or 1
+      vim.cmd("colorscheme sonokai")
+    end,
   },
-  config = function()
-    vim.g.gruvbox_material_background = "medium"
-    vim.g.gruvbox_material_foreground = "material"
-    vim.g.gruvbox_material_better_performance = 1
-    vim.cmd("colorscheme sonokai")
-  end,
-},
   { "nvim-tree/nvim-web-devicons", lazy = true },
 }
