5bcf40bae478b29d708442089314d81d42a25746
Žiga Hvalec <zigahvalec@gmail.com>
2026-03-19 21:32:42 +0100

    update structure, add auto reload, add new grd keymap

--- config/nvim/.config/nvim/init.lua
+++ config/nvim/.config/nvim/init.lua
@@ -1,27 +1,7 @@
 -- =========================
 -- Basic Options
 -- =========================
-vim.g.mapleader = " "             -- Space as leader
-vim.opt.clipboard = "unnamedplus" -- System clipboard
-vim.opt.tabstop = 2               -- Tab = 2 spaces
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.number = true         -- show absolute line number for current line
-vim.opt.relativenumber = true -- show relative numbers for all other lines
-vim.opt.ignorecase = true     -- ignore case when searching
-vim.opt.smartcase = true      -- but be case-sensitive if uppercase is used
-vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.signcolumn = "yes"    -- Always show the sign column to avoid text shifting when diagnostics appear
-vim.opt.scrolloff = 8         -- Keep eight lines visible above/below the cursor for better context while navigating
-vim.opt.updatetime = 200      -- Reduce the cursor-hold delay so LSP diagnostics/hover update more quickly
-vim.opt.swapfile = false      -- ignore swap files
-vim.opt.undofile = true       -- enable undofile history
-vim.diagnostic.config({
-  virtual_text = true,        -- show error text inline
-  underline = true,
-  signs = true,
-  update_in_insert = true, -- update while typing
-})
+require("common")
 
 -- =========================
 -- Bootstrap lazy.nvim
