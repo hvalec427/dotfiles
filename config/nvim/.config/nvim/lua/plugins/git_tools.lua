6cf6517024e5eff5c5e55fb9f1f5129a5755450e
Žiga Hvalec <zigahvalec@gmail.com>
2026-09-04 10:31:49 +0200

    nvim: drop gitsigns stage/reset keymaps (use lazygit for that)
    Keep only the read-only hunk keymaps: <leader>hp preview and <leader>hb
    blame. Staging/resetting hunks is handled in lazygit.

--- config/nvim/.config/nvim/lua/plugins/git_tools.lua
+++ config/nvim/.config/nvim/lua/plugins/git_tools.lua
@@ -39,17 +39,7 @@ return {
           return "<Ignore>"
         end, { expr = true, desc = "Prev hunk" })
 
-        -- Hunk actions
-        map("n", "<leader>hs", gs.stage_hunk, { desc = "[h]unk [s]tage" })
-        map("n", "<leader>hr", gs.reset_hunk, { desc = "[h]unk [r]eset" })
-        map("v", "<leader>hs", function()
-          gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "[h]unk [s]tage (selection)" })
-        map("v", "<leader>hr", function()
-          gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "[h]unk [r]eset (selection)" })
-        map("n", "<leader>hS", gs.stage_buffer, { desc = "[h]unk [S]tage buffer" })
-        map("n", "<leader>hR", gs.reset_buffer, { desc = "[h]unk [R]eset buffer" })
+        -- Hunk inspection (staging/reset is done in lazygit)
         map("n", "<leader>hp", gs.preview_hunk, { desc = "[h]unk [p]review" })
         map("n", "<leader>hb", function()
           gs.blame_line({ full = true })
