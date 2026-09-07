fb04cb928f9eda8cb9a5770fc883fad6948e4190
Žiga Hvalec <zigahvalec@gmail.com>
2026-09-07 16:05:36 +0200

    nvim: use default claudecode terminal size

--- config/nvim/.config/nvim/lua/plugins/claudecode.lua
+++ config/nvim/.config/nvim/lua/plugins/claudecode.lua
@@ -6,9 +6,6 @@ return {
       terminal = {
         provider = "snacks",
         snacks_win_opts = {
-          position = "float",
-          width = 0.9,
-          height = 0.9,
           border = "rounded",
           keys = {
             claude_hide = {
